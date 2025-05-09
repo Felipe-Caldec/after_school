eeed5a193d805186778f01be9840ab340db7835d
Felipe-Caldec <felipe.betocalderon@gmail.com>
2025-05-09 11:38:10 -0400

    variables entorno

--- after/settings.py
+++ after/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -125,4 +126,11 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_PASS')     # Contraseña de aplicación, no la normal
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
--- after_app/views.py
+++ after_app/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
 
 def portada(request):
+
+    if request.method == "POST":
+        nombre = request.POST.get("nombre")
+        apellido = request.POST.get("apellido")
+        correo = request.POST.get("correo")
+        telefono = request.POST.get("telefono")
+        mensaje = request.POST.get("mensaje")
+        fecha = request.POST.get("fecha")
+
+        asunto = "Nuevo mensaje de contacto"
+        cuerpo = (
+            f"Nombre: {nombre} {apellido}\n"
+            f"Correo: {correo}\n"
+            f"Teléfono: {telefono}\n"
+            f"Fecha: {fecha}\n"
+            f"Mensaje:\n{mensaje}"
+        )
+        send_mail(asunto, cuerpo, correo, ['felipe.betocalderon@gmail.com'])
+        return redirect("{% url 'portada' %}")
+    
     return render(request, 'portada.html')
 
 def talleres(request):
